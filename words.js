235b77897cd2aea27835089b11c3d336f60fd2d9
翁 <dennis1989999@gmail.com>
2025-03-15 14:57:49 +0800

    3/15更新

---
+++ words.js
@@ -0,0 +1,64 @@
+const wordsList = {
+    "3/14": [
+        { english: "counterfeit", chinese: "偽造", partOfSpeech: "verb/adjective" },
+        { english: "enquire", chinese: "詢問", partOfSpeech: "verb" },
+        { english: "dedicate", chinese: "奉獻", partOfSpeech: "verb" },
+        { english: "committed", chinese: "致力於", partOfSpeech: "adjective" }, // "commited" corrected to "committed"
+        { english: "mentor", chinese: "導師", partOfSpeech: "noun/verb" },
+        { english: "crucial", chinese: "關鍵的", partOfSpeech: "adjective" },
+        { english: "assist", chinese: "協助", partOfSpeech: "verb" },
+        { english: "peer", chinese: "同伴", partOfSpeech: "noun" },
+        { english: "inform", chinese: "通知", partOfSpeech: "verb" },
+        { english: "conform", chinese: "符合", partOfSpeech: "verb" },
+        { english: "material", chinese: "材料", partOfSpeech: "noun" }, // "matirial" corrected to "material"
+        { english: "adhere", chinese: "堅持", partOfSpeech: "verb" },
+        { english: "beckon", chinese: "招手", partOfSpeech: "verb" }, // "bexkon" corrected to "beckon"
+        { english: "interior", chinese: "內部", partOfSpeech: "noun/adjective" },
+        { english: "statement", chinese: "陳述", partOfSpeech: "noun" }, // "ststement" corrected to "statement"
+        { english: "backing", chinese: "支持", partOfSpeech: "noun" },
+        { english: "hardly", chinese: "幾乎不", partOfSpeech: "adverb" },
+        { english: "nearly", chinese: "幾乎", partOfSpeech: "adverb" },
+        { english: "merely", chinese: "僅僅", partOfSpeech: "adverb" },
+        { english: "barely", chinese: "勉強", partOfSpeech: "adverb" },
+        { english: "regain", chinese: "恢復", partOfSpeech: "verb" },
+        { english: "underwent", chinese: "經歷", partOfSpeech: "verb" },
+        { english: "renovation", chinese: "翻新", partOfSpeech: "noun" },
+        { english: "audit", chinese: "審計", partOfSpeech: "noun/verb" },
+        { english: "dispose", chinese: "處理", partOfSpeech: "verb" },
+        { english: "tenancy", chinese: "租賃", partOfSpeech: "noun" },
+        { english: "premise", chinese: "前提", partOfSpeech: "noun" },
+        { english: "commitment", chinese: "承諾", partOfSpeech: "noun" },
+        { english: "obliged", chinese: "有義務的", partOfSpeech: "adjective" }, // "obligged" corrected to "obliged"
+        { english: "plunged", chinese: "猛跌", partOfSpeech: "verb" },
+        { english: "demand", chinese: "需求", partOfSpeech: "noun/verb" },
+        { english: "exclusion", chinese: "排除", partOfSpeech: "noun" },
+        { english: "instability", chinese: "不穩定", partOfSpeech: "noun" },
+        { english: "evolve", chinese: "進化", partOfSpeech: "verb" },
+        { english: "involve", chinese: "涉及", partOfSpeech: "verb" },
+        { english: "invoke", chinese: "調用", partOfSpeech: "verb" },
+        { english: "conscious", chinese: "有意識的", partOfSpeech: "adjective" }, // "consious" corrected to "conscious"
+        { english: "cozy", chinese: "舒適的", partOfSpeech: "adjective" },
+        { english: "exotic", chinese: "異國的", partOfSpeech: "adjective" },
+        { english: "receptive", chinese: "接受的", partOfSpeech: "adjective" },
+        { english: "decline", chinese: "衰退", partOfSpeech: "verb/noun" },
+        { english: "discretion", chinese: "審慎", partOfSpeech: "noun" },
+        { english: "subjective", chinese: "主觀的", partOfSpeech: "adjective" },
+        { english: "objective", chinese: "客觀的", partOfSpeech: "adjective" },
+        { english: "hinder", chinese: "阻礙", partOfSpeech: "verb" },
+        { english: "on account of", chinese: "由於", partOfSpeech: "preposition" },
+        { english: "conduct", chinese: "行為", partOfSpeech: "noun/verb" },
+        { english: "subordinate", chinese: "下屬", partOfSpeech: "noun/adjective" },
+        { english: "overlapped", chinese: "重疊", partOfSpeech: "verb" },
+        { english: "marginalized", chinese: "邊緣化", partOfSpeech: "adjective" }, // "amarginalized" corrected to "marginalized"
+        { english: "exaggerated", chinese: "誇張的", partOfSpeech: "adjective" },
+        { english: "vanquished", chinese: "被征服的", partOfSpeech: "verb/adjective" },
+        { english: "regardless of", chinese: "不管", partOfSpeech: "preposition" },
+        { english: "punctually", chinese: "準時地", partOfSpeech: "adverb" },
+        { english: "with respect to", chinese: "關於", partOfSpeech: "preposition" }, // "with respective of" corrected to "with respect to"
+    ],
+    // 之後可以添加其他日期的單字列表
+    "3/15": [],
+    "3/16": [],
+};
+
+export default wordsList;
